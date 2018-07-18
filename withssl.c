aa89c5f711113231b590fd46c4987deea88f610a
Matthieu Poullet <matthieu.poullet@gmail.com>
2018-07-18 15:31:02 +0200

    Apply clang-format.

--- withssl.c
+++ withssl.c
@@ -1,81 +1,79 @@
-#include "openssl/ssl.h"
 #include "openssl/bio.h"
 #include "openssl/err.h"
+#include "openssl/ssl.h"
 
 #include "stdio.h"
 #include "string.h"
 
-int main()
-{
-    BIO * bio;
-    SSL * ssl;
-    SSL_CTX * ctx;
+int main() {
+  BIO* bio;
+  SSL* ssl;
+  SSL_CTX* ctx;
 
-    int p;
+  int p;
 
-    char * request = "GET / HTTP/1.1\x0D\x0AHost: www.verisign.com\x0D\x0A\x43onnection: Close\x0D\x0A\x0D\x0A";
-    char r[1024];
+  // clang-format off
+  char * request = "GET / HTTP/1.1\x0D\x0AHost: www.verisign.com\x0D\x0A\x43onnection: Close\x0D\x0A\x0D\x0A";
+  // clang-format on
+  char r[1024];
 
-    /* Set up the library */
-    ERR_load_BIO_strings();
-    SSL_load_error_strings();
-    SSL_library_init();
-    OpenSSL_add_all_algorithms();
+  /* Set up the library */
+  ERR_load_BIO_strings();
+  SSL_load_error_strings();
+  SSL_library_init();
+  OpenSSL_add_all_algorithms();
 
-    /* Set up the SSL context */
-    ctx = SSL_CTX_new(SSLv23_client_method());
+  /* Set up the SSL context */
+  ctx = SSL_CTX_new(SSLv23_client_method());
 
-    /* Load the trust store */
-    if(! SSL_CTX_load_verify_locations(ctx, "TrustStore.pem", NULL))
-    {
-        fprintf(stderr, "Error loading trust store\n");
-        ERR_print_errors_fp(stderr);
-        SSL_CTX_free(ctx);
-        return -1;
-    }
+  /* Load the trust store */
+  if (!SSL_CTX_load_verify_locations(ctx, "TrustStore.pem", NULL)) {
+    fprintf(stderr, "Error loading trust store\n");
+    ERR_print_errors_fp(stderr);
+    SSL_CTX_free(ctx);
+    return -1;
+  }
 
-    /* Setup the connection */
-    bio = BIO_new_ssl_connect(ctx);
+  /* Setup the connection */
+  bio = BIO_new_ssl_connect(ctx);
 
-    /* Set the SSL_MODE_AUTO_RETRY flag */
-    BIO_get_ssl(bio, & ssl);
-    SSL_set_mode(ssl, SSL_MODE_AUTO_RETRY);
+  /* Set the SSL_MODE_AUTO_RETRY flag */
+  BIO_get_ssl(bio, &ssl);
+  SSL_set_mode(ssl, SSL_MODE_AUTO_RETRY);
 
-    /* Create and setup the connection */
-    BIO_set_conn_hostname(bio, "www.verisign.com:https");
+  /* Create and setup the connection */
+  BIO_set_conn_hostname(bio, "www.verisign.com:https");
 
-    if(BIO_do_connect(bio) <= 0)
-    {
-        fprintf(stderr, "Error attempting to connect\n");
-        ERR_print_errors_fp(stderr);
-        BIO_free_all(bio);
-        SSL_CTX_free(ctx);
-        return -2;
-    }
-
-    /* Check the certificate */
-    if(SSL_get_verify_result(ssl) != X509_V_OK)
-    {
-        fprintf(stderr, "Certificate verification error: %ld\n", SSL_get_verify_result(ssl));
-        BIO_free_all(bio);
-        SSL_CTX_free(ctx);
-        return -3;
-    }
-
-    /* Send the request */
-    BIO_write(bio, request, strlen(request));
-
-    /* Read in the response */
-    for(;;)
-    {
-        p = BIO_read(bio, r, 1023);
-        if(p <= 0) break;
-        r[p] = 0;
-        printf("%s", r);
-    }
-
-    /* Close the connection and free the context */
+  if (BIO_do_connect(bio) <= 0) {
+    fprintf(stderr, "Error attempting to connect\n");
+    ERR_print_errors_fp(stderr);
     BIO_free_all(bio);
     SSL_CTX_free(ctx);
-    return 0;
+    return -2;
+  }
+
+  /* Check the certificate */
+  if (SSL_get_verify_result(ssl) != X509_V_OK) {
+    fprintf(stderr, "Certificate verification error: %ld\n",
+            SSL_get_verify_result(ssl));
+    BIO_free_all(bio);
+    SSL_CTX_free(ctx);
+    return -3;
+  }
+
+  /* Send the request */
+  BIO_write(bio, request, strlen(request));
+
+  /* Read in the response */
+  for (;;) {
+    p = BIO_read(bio, r, 1023);
+    if (p <= 0) break;
+    r[p] = 0;
+    printf("%s", r);
+  }
+
+  /* Close the connection and free the context */
+  BIO_free_all(bio);
+  SSL_CTX_free(ctx);
+  return 0;
 }
